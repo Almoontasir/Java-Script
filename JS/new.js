e220d729f7b4676fbf53bb547fbbd6afd11fa567
Md.Almoontasir Moon <75898108+Almoontasir@users.noreply.github.com>
2023-12-12 23:09:50 +0600

    Add files via upload

---
+++ JS/new.js
@@ -0,0 +1,11 @@
+        //  console.log(alert("Don't mess up with me!"));
+        // let age = prompt("What is your age?",18);
+        if(age>=18)
+           console.log("You are eligible to enter the page");
+        else
+        console.log("Yor are under 18 ,not eligible to watch 18+ content");   
+        let cookies = confirm("Do you really want to allow us cookies");
+        if(cookies)
+            console.log("Thank You for being a friend!");
+        else
+            console.log("Yor are not a good person!");
